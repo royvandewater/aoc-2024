80bb254670cc0a3da03e2deb7bf69ead7ea12f78
Roy van de Water <roy.v.water@gmail.com>
2025-12-27 10:34:00 -0900

    Day 4, part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,7 +3,7 @@ use std::fs::read_to_string;
 fn main() {
     let input = read_to_string("./input.txt").unwrap();
     println!("part_1: {}", part_1(&input));
-    // println!("part_2: {}", part_2(&input));
+    println!("part_2: {}", part_2(&input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -28,6 +28,25 @@ fn part_1(input: &str) -> usize {
         .sum()
 }
 
+fn part_2(input: &str) -> usize {
+    let lines: Vec<Vec<char>> = input
+        .trim()
+        .lines()
+        .map(|l| l.trim().chars().collect::<Vec<char>>())
+        .collect();
+
+    lines
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            line.iter()
+                .enumerate()
+                .map(|(x, _)| count_x_mas(&lines, x.try_into().unwrap(), y.try_into().unwrap()))
+                .sum::<usize>()
+        })
+        .sum()
+}
+
 fn count_xmas(lines: &Vec<Vec<char>>, x: isize, y: isize) -> usize {
     xmas_n(lines, x, y)
         + xmas_ne(lines, x, y)
@@ -37,6 +56,22 @@ fn count_xmas(lines: &Vec<Vec<char>>, x: isize, y: isize) -> usize {
         + xmas_sw(lines, x, y)
         + xmas_w(lines, x, y)
         + xmas_nw(lines, x, y)
+}
+
+fn count_x_mas(lines: &Vec<Vec<char>>, x: isize, y: isize) -> usize {
+    let c = character_at(lines, x, y);
+    let nw = character_at(lines, x - 1, y - 1);
+    let ne = character_at(lines, x + 1, y - 1);
+    let sw = character_at(lines, x - 1, y + 1);
+    let se = character_at(lines, x + 1, y + 1);
+
+    match ((nw, c, se), (sw, c, ne)) {
+        (('M', 'A', 'S'), ('M', 'A', 'S')) => 1,
+        (('M', 'A', 'S'), ('S', 'A', 'M')) => 1,
+        (('S', 'A', 'M'), ('M', 'A', 'S')) => 1,
+        (('S', 'A', 'M'), ('S', 'A', 'M')) => 1,
+        _ => 0,
+    }
 }
 
 fn xmas_n(lines: &Vec<Vec<char>>, x: isize, y: isize) -> usize {
@@ -133,6 +168,22 @@ mod tests {
             ..M.M.M.MM
             .X.X.XMASX";
         assert_eq!(part_1(&input), 18);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "
+            .M.S......
+            ..A..MSMS.
+            .M.S.MAA..
+            ..A.ASMSM.
+            .M.S.M....
+            ..........
+            S.S.S.S.S.
+            .A.A.A.A..
+            M.M.M.M.M.
+            ..........";
+        assert_eq!(part_2(&input), 9);
     }
 
     #[test]
