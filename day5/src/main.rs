e045ed00fd15cbf6ca028825045047a7f3cb289d
Roy van de Water <roy.v.water@gmail.com>
2025-12-27 12:24:03 -0900

    Day 5, part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,7 +4,7 @@ use std::{collections::HashSet, fs::read_to_string};
 fn main() {
     let input = read_to_string("./input.txt").unwrap();
     println!("part_1: {}", part_1(&input));
-    // println!("part_2: {}", part_2(&input));
+    println!("part_2: {}", part_2(&input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -12,14 +12,25 @@ fn part_1(input: &str) -> usize {
 
     updates
         .iter()
-        .filter(|update| valid_update(&rules, update))
+        .filter(|update| is_valid_update(&rules, update))
+        .map(|update| middle_page(&update))
+        .sum()
+}
+
+fn part_2(input: &str) -> usize {
+    let (rules, updates) = parse_updates_and_rules(input).unwrap();
+
+    updates
+        .iter()
+        .filter(|update| is_not_valid_update(&rules, update))
+        .map(|update| fix_update(&rules, &update))
         .map(|update| middle_page(&update))
         .sum()
 }
 
 fn middle_page(update: &Vec<usize>) -> usize {
     let length = update.len();
-    assert_eq!(length % 2, 1); // length should always be odd
+    assert_eq!(length % 2, 1, "Update length was not odd: {}", length);
     let i = length / 2;
 
     *update.iter().nth(i).unwrap()
@@ -58,7 +69,11 @@ fn parse_int(s: &str) -> Result<usize, Box<dyn Error>> {
     Ok(i)
 }
 
-fn valid_update(rules: &Vec<(usize, usize)>, update: &Vec<usize>) -> bool {
+fn is_not_valid_update(rules: &Vec<(usize, usize)>, update: &Vec<usize>) -> bool {
+    !is_valid_update(rules, update)
+}
+
+fn is_valid_update(rules: &Vec<(usize, usize)>, update: &Vec<usize>) -> bool {
     update.iter().enumerate().all(|(i, page)| {
         let preceding_pages: HashSet<usize> = update.iter().take(i).map(|p| p.clone()).collect();
 
@@ -75,6 +90,35 @@ fn valid_update(rules: &Vec<(usize, usize)>, update: &Vec<usize>) -> bool {
     })
 }
 
+fn rules_is_valid_for_update(rule: &(usize, usize), update: &Vec<usize>) -> bool {
+    let (a, b) = rule;
+    let ai = update.iter().position(|p| p == a);
+    let bi = update.iter().position(|p| p == b);
+
+    match (ai, bi) {
+        (Some(ai), Some(bi)) => ai < bi,
+        _ => true,
+    }
+}
+
+fn fix_update(rules: &Vec<(usize, usize)>, update: &Vec<usize>) -> Vec<usize> {
+    let invalid_rule = rules
+        .iter()
+        .find(|rule| !rules_is_valid_for_update(rule, update));
+
+    match invalid_rule {
+        None => update.clone(),
+        Some((a, b)) => {
+            let ai = update.iter().position(|p| p == a).unwrap();
+            let bi = update.iter().position(|p| p == b).unwrap();
+
+            let mut update = update.clone();
+            update.swap(ai, bi);
+            fix_update(rules, &update)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -87,20 +131,26 @@ mod tests {
     }
 
     #[test]
+    fn test_part_2() {
+        let input = read_to_string("./example_input.txt").unwrap();
+        assert_eq!(part_2(&input), 123);
+    }
+
+    #[test]
     fn test_valid_update_when_empty() {
-        let result = valid_update(&vec![], &vec![]);
+        let result = is_valid_update(&vec![], &vec![]);
         assert!(result)
     }
 
     #[test]
     fn test_valid_update_when_one_rule_one_update() {
-        let result = valid_update(&vec![(1, 2)], &vec![1, 2]);
+        let result = is_valid_update(&vec![(1, 2)], &vec![1, 2]);
         assert!(result)
     }
 
     #[test]
     fn test_valid_update_when_one_rule_one_invalid_update() {
-        let result = valid_update(&vec![(1, 2)], &vec![2, 1]);
+        let result = is_valid_update(&vec![(1, 2)], &vec![2, 1]);
         assert!(!result)
     }
 
@@ -110,7 +160,7 @@ mod tests {
         let (rules, updates) = parse_updates_and_rules(&input).unwrap();
         let update = updates.iter().nth(0).unwrap();
 
-        let result = valid_update(&rules, &update);
+        let result = is_valid_update(&rules, &update);
         assert!(result);
     }
 
@@ -120,7 +170,7 @@ mod tests {
         let (rules, updates) = parse_updates_and_rules(&input).unwrap();
         let update = updates.iter().nth(3).unwrap();
 
-        let result = valid_update(&rules, &update);
+        let result = is_valid_update(&rules, &update);
         assert!(!result);
     }
 
@@ -130,7 +180,7 @@ mod tests {
         let (rules, updates) = parse_updates_and_rules(&input).unwrap();
         let update = updates.iter().nth(3).unwrap();
 
-        let result = valid_update(&rules, &update);
+        let result = is_valid_update(&rules, &update);
         assert!(!result);
     }
 
@@ -138,5 +188,15 @@ mod tests {
     fn test_middle_page() {
         let result = middle_page(&vec![1, 2, 3]);
         assert_eq!(result, 2)
+    }
+
+    #[test]
+    fn test_fix_update() {
+        let input = read_to_string("./example_input.txt").unwrap();
+        let (rules, _) = parse_updates_and_rules(&input).unwrap();
+        let update: Vec<usize> = vec![75, 97, 47, 61, 53];
+
+        let result = fix_update(&rules, &update);
+        assert_eq!(result, vec![97, 75, 47, 61, 53])
     }
 }
