f3cd5ca1e331685c17f0481d76a1e3035118434d
Roy van de Water <roy.v.water@gmail.com>
2025-12-28 10:49:32 -0900

    Refactor part_1 to return a trail rather than altering the map

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,15 +1,25 @@
-use std::fs::read_to_string;
+use std::{collections::HashSet, fs::read_to_string};
 
 fn main() {
     let input = read_to_string("./input.txt").unwrap();
 
     println!("part_1: {}", part_1(&input));
+    println!("part_2: {}", part_2(&input));
 }
 
 fn part_1(input: &str) -> usize {
     let input = trim_lines(input);
-    let output = recursively_step(&input);
-    output.chars().filter(|c| *c == 'X').count()
+    let guard = find_guard(&input).unwrap();
+    let trail = recursively_step(&input, &vec![guard]);
+    let positions: HashSet<_> = trail.iter().map(|g| g.pos()).collect();
+    positions.len()
+}
+
+fn part_2(input: &str) -> usize {
+    let input = trim_lines(input);
+    let guard = find_guard(&input).unwrap();
+    let trail = recursively_step(&input, &vec![guard]);
+    trail.len()
 }
 
 fn trim_lines(input: &str) -> String {
@@ -21,88 +31,89 @@ fn trim_lines(input: &str) -> String {
         .join("\n")
 }
 
-fn recursively_step(input: &str) -> String {
-    match step(input) {
-        None => input.to_string(),
-        Some(next) => recursively_step(&next),
-    }
-}
+fn recursively_step(input: &str, trail: &Vec<Guard>) -> Vec<Guard> {
+    assert!(!trail.is_empty(), "trail must not be empty");
+    let guard = trail.last().unwrap();
 
-fn step(input: &str) -> Option<String> {
-    let pos = find_guard_position(input)?;
-    let guard = get_char_at_pos(input, &pos)?;
-
-    let next_pos = get_next_pos(input, guard, &pos);
-    if next_pos.is_none() {
-        return Some(insert_char_at(input, &pos, 'X'));
-    }
-
-    let next_pos = next_pos.unwrap();
-    match get_char_at_pos(input, &next_pos).unwrap() {
-        '#' => Some(rotate_guard(input, &pos, guard)),
-        _ => {
-            let guard_replaced = insert_char_at(input, &pos, 'X');
-            let output = insert_char_at(&guard_replaced, &next_pos, guard);
-            Some(output)
+    match step(input, guard) {
+        None => trail.clone(),
+        Some(next_guard) => {
+            let mut next_trail = trail.clone();
+            next_trail.push(next_guard);
+            recursively_step(&input, &next_trail)
         }
     }
 }
 
-fn rotate_guard(input: &str, pos: &(usize, usize), guard: char) -> String {
-    let next_guard = match guard {
-        '^' => '>',
-        '>' => 'v',
-        'v' => '<',
-        '<' => '^',
-        _ => panic!("Unrecognized guard rotation: {}", guard),
-    };
+// returns None when the guard has left the map
+fn step(input: &str, guard: &Guard) -> Option<Guard> {
+    let next_guard = guard.advance()?;
+    let next_tile = get_char_at_pos(input, &next_guard.pos())?;
 
-    insert_char_at(input, pos, next_guard)
-}
-
-fn insert_char_at(input: &str, pos: &(usize, usize), n: char) -> String {
-    input
-        .lines()
-        .enumerate()
-        .map(|(y, line)| match y == pos.1 {
-            false => line.to_string(),
-            true => line
-                .chars()
-                .enumerate()
-                .map(|(x, c)| match x {
-                    x if x == pos.0 => n,
-                    _ => c,
-                })
-                .collect(),
-        })
-        .collect::<Vec<_>>()
-        .join("\n")
+    match next_tile {
+        '#' => step(input, &guard.rotate()),
+        _ => Some(next_guard),
+    }
 }
 
 fn get_char_at_pos(input: &str, (x, y): &(usize, usize)) -> Option<char> {
     input.lines().nth(*y)?.chars().nth(*x)
 }
 
-fn get_next_pos(input: &str, guard: char, (x, y): &(usize, usize)) -> Option<(usize, usize)> {
-    let (x0, y0) = (isize::try_from(*x).unwrap(), isize::try_from(*y).unwrap());
+#[derive(Clone, Debug)]
+enum Guard {
+    North((usize, usize)),
+    East((usize, usize)),
+    South((usize, usize)),
+    West((usize, usize)),
+}
 
-    let (x1, y1) = match guard {
-        '^' => (x0, y0 - 1),
-        '>' => (x0 + 1, y0),
-        'v' => (x0, y0 + 1),
-        '<' => (x0 - 1, y0),
-        _ => panic!("Unrecognized guard character: {}", guard),
-    };
-
-    let max_y = isize::try_from(input.lines().count() - 1).unwrap();
-    let max_x = isize::try_from(input.lines().nth(0).unwrap_or("").len() - 1).unwrap();
-
-    if x1 < 0 || max_x < x1 || y1 < 0 || max_y < y1 {
-        return None;
+impl Guard {
+    fn rotate(&self) -> Guard {
+        match self {
+            Guard::North(p) => Guard::East(*p),
+            Guard::East(p) => Guard::South(*p),
+            Guard::South(p) => Guard::West(*p),
+            Guard::West(p) => Guard::North(*p),
+        }
     }
 
-    let (x1, y1) = (usize::try_from(x1).unwrap(), usize::try_from(y1).unwrap());
-    return Some((x1, y1));
+    fn pos(&self) -> (usize, usize) {
+        match self {
+            Guard::North(p) => *p,
+            Guard::East(p) => *p,
+            Guard::South(p) => *p,
+            Guard::West(p) => *p,
+        }
+    }
+
+    fn advance(&self) -> Option<Guard> {
+        match self.clone() {
+            Guard::North((_, y)) if y == 0 => return None,
+            Guard::West((x, _)) if x == 0 => return None,
+            _ => {}
+        }
+
+        Some(match self.clone() {
+            Guard::North((x, y)) => Guard::North((x, y - 1)),
+            Guard::East((x, y)) => Guard::East((x + 1, y)),
+            Guard::South((x, y)) => Guard::South((x, y + 1)),
+            Guard::West((x, y)) => Guard::West((x - 1, y)),
+        })
+    }
+}
+
+fn find_guard(input: &str) -> Option<Guard> {
+    let pos = find_guard_position(input)?;
+    let c = get_char_at_pos(input, &pos).unwrap();
+
+    match c {
+        '^' => Some(Guard::North(pos)),
+        '>' => Some(Guard::West(pos)),
+        'v' => Some(Guard::South(pos)),
+        '<' => Some(Guard::East(pos)),
+        _ => panic!("Unrecognized guard position: {} ({}, {})", c, pos.0, pos.1),
+    }
 }
 
 fn find_guard_position(input: &str) -> Option<(usize, usize)> {
@@ -136,10 +147,10 @@ mod tests {
         assert_eq!(part_1(&input), 41);
     }
 
+    #[ignore]
     #[test]
-    fn test_get_next_pos() {
-        let input = trim_lines(
-            "
+    fn test_part_2_example() {
+        let input = "
             ....#.....
             .........#
             ..........
@@ -149,10 +160,8 @@ mod tests {
             .#..^.....
             ........#.
             #.........
-            ......#...",
-        );
+            ......#...";
 
-        let result = get_next_pos(&input, '^', &(4, 6)).unwrap();
-        assert_eq!(result, (4, 5));
+        assert_eq!(part_2(&input), 6);
     }
 }
