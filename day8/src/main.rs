ace0c19f712eaeecbe322648b1fb218f55ae1310
Roy van de Water <roy.v.water@gmail.com>
2026-01-02 11:24:09 -0900

    Day 8, part 1

---
+++ day8/src/main.rs
@@ -0,0 +1,197 @@
+use itertools::Itertools;
+use std::{
+    collections::{HashMap, HashSet},
+    fs::read_to_string,
+};
+
+fn main() {
+    let input = read_to_string("./input.txt").unwrap();
+
+    println!("part_1: {}", part_1(&input));
+}
+
+// currently returning 396, which is too high
+fn part_1(input: &str) -> usize {
+    let (width, height) = dimensions(&input);
+    assert!(width > 0, "Width must be greater than 0");
+    assert!(height > 0, "height must be greater than 0");
+
+    let (max_x, max_y) = (width - 1, height - 1);
+
+    parse_input(&input)
+        .values()
+        .flat_map(|antennas| find_antinodes_for_all_antennas((max_x, max_y), antennas))
+        .collect::<HashSet<(usize, usize)>>()
+        .len()
+}
+
+fn parse_input(input: &str) -> HashMap<char, Vec<(usize, usize)>> {
+    trim_whitespace(input)
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| line.chars().enumerate().map(move |(x, c)| (c, (x, y))))
+        .filter(|(c, _)| *c != '.')
+        .into_group_map()
+}
+
+fn find_antinodes_for_all_antennas(
+    (max_x, max_y): (usize, usize),
+    antennas: &Vec<(usize, usize)>,
+) -> HashSet<(usize, usize)> {
+    antennas
+        .iter()
+        .combinations(2)
+        .flat_map(|pair| {
+            let (a, b) = pair.iter().collect_tuple().unwrap();
+            find_antinodes_for_pair((max_x, max_y), **a, **b)
+        })
+        .collect()
+}
+
+fn find_antinodes_for_pair(
+    (max_x, max_y): (usize, usize),
+    (x1, y1): (usize, usize),
+    (x2, y2): (usize, usize),
+) -> HashSet<(usize, usize)> {
+    let (max_x, max_y) = u_to_i((max_x, max_y));
+    let (x1, y1) = u_to_i((x1, y1));
+    let (x2, y2) = u_to_i((x2, y2));
+
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+
+    HashSet::from([(x1 - dx, y1 - dy), (x2 + dx, y2 + dy)])
+        .into_iter()
+        .filter(|(x, y)| 0 <= *x && 0 <= *y)
+        .filter(|(x, y)| *x <= max_x && *y <= max_y)
+        .map(|c| i_to_u(c))
+        .collect()
+}
+
+fn i_to_u((x, y): (isize, isize)) -> (usize, usize) {
+    (usize::try_from(x).unwrap(), usize::try_from(y).unwrap())
+}
+
+fn u_to_i((x, y): (usize, usize)) -> (isize, isize) {
+    (isize::try_from(x).unwrap(), isize::try_from(y).unwrap())
+}
+
+// Returns the width & height of the map
+fn dimensions(input: &str) -> (usize, usize) {
+    let input = trim_whitespace(input);
+
+    match input.lines().count() {
+        0 => (0, 0),
+        height => (input.lines().nth(0).unwrap().len(), height),
+    }
+}
+
+// trims whitespace from the whole map and from each line
+fn trim_whitespace(input: &str) -> String {
+    input
+        .trim()
+        .lines()
+        .map(|l| l.trim())
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part_1_example() {
+        let input = read_to_string("./input_example.txt").unwrap();
+
+        assert_eq!(part_1(&input), 14)
+    }
+
+    #[test]
+    fn test_find_antinodes_for_all_antennas_when_empty() {
+        let result = find_antinodes_for_all_antennas((0, 0), &vec![]);
+        assert_eq!(result, HashSet::new())
+    }
+
+    #[test]
+    fn test_find_antinodes_for_all_antennas_when_one() {
+        let result = find_antinodes_for_all_antennas((0, 0), &vec![(0, 0)]);
+        assert_eq!(result, HashSet::new())
+    }
+
+    #[test]
+    fn test_find_antinodes_for_all_antennas_when_two() {
+        let result = find_antinodes_for_all_antennas((3, 3), &vec![(1, 1), (2, 2)]);
+        assert_eq!(result, HashSet::from([(0, 0), (3, 3)]))
+    }
+
+    #[test]
+    fn test_find_antinodes_for_all_antennas_when_three() {
+        // .#.#
+        // #aa#
+        // .a..
+        // ##..
+
+        let result = find_antinodes_for_all_antennas((3, 3), &vec![(1, 1), (1, 2), (2, 1)]);
+        let mut result: Vec<(usize, usize)> = result.iter().cloned().collect();
+        result.sort();
+
+        assert_eq!(result, vec![(0, 1), (0, 3), (1, 0), (1, 3), (3, 0), (3, 1)],)
+    }
+
+    #[test]
+    fn test_find_antinodes_for_pair_one_apart() {
+        let result = find_antinodes_for_pair((3, 3), (1, 1), (2, 2));
+
+        assert_eq!(result, HashSet::from([(0, 0), (3, 3)]))
+    }
+
+    #[test]
+    fn test_find_antinodes_for_pair_in_a_row() {
+        let result = find_antinodes_for_pair((3, 3), (1, 1), (2, 1));
+
+        assert_eq!(result, HashSet::from([(0, 1), (3, 1)]))
+    }
+
+    #[test]
+    fn test_find_antinodes_for_pair_in_a_row_opposite_order() {
+        let result = find_antinodes_for_pair((3, 3), (2, 1), (1, 1));
+
+        assert_eq!(result, HashSet::from([(0, 1), (3, 1)]))
+    }
+
+    #[test]
+    fn test_find_antinodes_for_pair_in_a_column() {
+        let result = find_antinodes_for_pair((3, 3), (1, 1), (1, 2));
+
+        assert_eq!(result, HashSet::from([(1, 0), (1, 3)]))
+    }
+
+    #[test]
+    fn test_find_antinodes_for_pair_different_arrangement() {
+        let result = find_antinodes_for_pair((3, 3), (1, 2), (2, 1));
+
+        assert_eq!(result, HashSet::from([(3, 0), (0, 3)]))
+    }
+
+    #[test]
+    fn test_find_antinodes_for_pair_different_arrangement_opposite_order() {
+        let result = find_antinodes_for_pair((3, 3), (2, 1), (1, 2));
+
+        assert_eq!(result, HashSet::from([(3, 0), (0, 3)]))
+    }
+
+    #[test]
+    fn test_find_antinodes_for_pair_one_off_map() {
+        let result = find_antinodes_for_pair((2, 2), (1, 1), (2, 2));
+
+        assert_eq!(result, HashSet::from([(0, 0)]))
+    }
+
+    #[test]
+    fn test_find_antinodes_for_pair_goes_negative() {
+        let result = find_antinodes_for_pair((2, 2), (0, 0), (1, 1));
+
+        assert_eq!(result, HashSet::from([(2, 2)]))
+    }
+}
