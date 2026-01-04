893417f8ea355c97477879186dfb9c2c29e0de7f
Roy van de Water <roy.v.water@gmail.com>
2026-01-04 09:42:07 -0900

    Day 11, part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,23 +1,36 @@
 mod step;
 
-use std::fs::read_to_string;
+use std::{fs::read_to_string, num::ParseIntError};
 use step::step;
 
 fn main() {
     let input = read_to_string("input.txt").unwrap();
 
     println!("part_1: {}", part_1(&input));
+    println!("part_2: {}", part_2(&input));
 }
 
 fn part_1(input: &str) -> usize {
-    let stones: Vec<usize> = input
+    let stones = parse_stones(input).unwrap();
+    let stones = (0..25).fold(stones, |acc, _| step(acc));
+    stones.iter().map(|(_, n)| n).sum()
+}
+
+fn part_2(input: &str) -> usize {
+    let stones = parse_stones(input).unwrap();
+    let stones = (0..75).fold(stones, |acc, _| step(acc));
+    stones.iter().map(|(_, n)| n).sum()
+}
+
+fn parse_stones(input: &str) -> Result<Vec<(usize, usize)>, ParseIntError> {
+    Ok(input
         .trim()
         .split_whitespace()
-        .map(|number| number.parse().unwrap())
-        .collect();
-
-    let stones = (0..25).fold(stones, |acc, _| step(&acc));
-    stones.len()
+        .map(|number| number.parse::<usize>())
+        .collect::<Result<Vec<usize>, _>>()?
+        .into_iter()
+        .map(|x| (x, 1))
+        .collect::<Vec<(usize, usize)>>())
 }
 
 #[cfg(test)]
--- day11/src/step.rs
+++ day11/src/step.rs
@@ -1,38 +1,88 @@
-pub fn step(stones: &Vec<usize>) -> Vec<usize> {
-    stones.iter().flat_map(step_stone).collect()
+use itertools::Itertools;
+
+// (stone_digit, number_of_stones_it_appears_on)
+type Stone = (usize, usize);
+
+pub fn step(stones: Vec<Stone>) -> Vec<Stone> {
+    stones
+        .iter()
+        .flat_map(|stone| step_stone(*stone))
+        .into_group_map()
+        .iter()
+        .map(|(x, ns)| (*x, ns.iter().sum()))
+        .collect()
 }
 
-fn step_stone(stone: &usize) -> Vec<usize> {
+fn step_stone(stone: Stone) -> Vec<Stone> {
     match stone {
-        0 => vec![1],
-        x if has_even_digits(stone) => split_stone(stone),
-        x => vec![x * 2024],
+        (0, n) => vec![(1, n)],
+        (x, n) if has_even_digits(x) => split_stone(x, n),
+        (x, n) => vec![(x * 2024, n)],
     }
 }
 
-fn has_even_digits(stone: &usize) -> bool {
-    format!("{}", stone).chars().count().is_multiple_of(2)
+fn has_even_digits(x: usize) -> bool {
+    format!("{}", x).chars().count().is_multiple_of(2)
 }
 
-fn split_stone(stone: &usize) -> Vec<usize> {
-    let stone_str = format!("{}", stone);
+fn split_stone(x: usize, n: usize) -> Vec<Stone> {
+    let stone_str = format!("{}", x);
     let mid = stone_str.chars().count() / 2;
     let (head, tail) = stone_str.split_at(mid);
 
-    vec![head.parse().unwrap(), tail.parse().unwrap()]
+    vec![(head.parse().unwrap(), n), (tail.parse().unwrap(), n)]
 }
 
 #[cfg(test)]
 mod test {
+    use std::collections::HashSet;
+
     use super::*;
 
     #[test]
     fn test_step_empty() {
-        assert_eq!(step(&vec![]), vec![]);
+        assert_eq!(step(vec![]), vec![]);
     }
 
     #[test]
     fn test_step_0() {
-        assert_eq!(step(&vec![0]), vec![1]);
+        assert_eq!(step(vec![(0, 1)]), vec![(1, 1)]);
+    }
+
+    #[test]
+    fn test_step_0_with_n_2() {
+        assert_eq!(step(vec![(0, 2)]), vec![(1, 2)]);
+    }
+
+    #[test]
+    fn test_step_1() {
+        assert_eq!(step(vec![(1, 1)]), vec![(2024, 1)]);
+    }
+
+    #[test]
+    fn test_step_1_n_2() {
+        assert_eq!(step(vec![(1, 2)]), vec![(2024, 2)]);
+    }
+
+    #[test]
+    fn test_step_10() {
+        let result: HashSet<Stone> = step(vec![(10, 1)]).iter().cloned().collect();
+        assert_eq!(result, HashSet::from([(0, 1), (1, 1)]));
+    }
+
+    #[test]
+    fn test_step_2_stones_with_0() {
+        assert_eq!(step(vec![(0, 1), (0, 1)]), vec![(1, 2)]);
+    }
+
+    #[test]
+    fn test_step_2_stones_with_1() {
+        assert_eq!(step(vec![(1, 1), (1, 1)]), vec![(2024, 2)]);
+    }
+
+    #[test]
+    fn test_step_2_stones_with_10() {
+        let result: HashSet<Stone> = step(vec![(10, 1), (10, 1)]).iter().cloned().collect();
+        assert_eq!(result, HashSet::from([(0, 2), (1, 2)]));
     }
 }
